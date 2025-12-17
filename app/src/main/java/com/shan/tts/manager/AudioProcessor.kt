ba2bdaede8afca46e167a72afaf95fbfc15b7b0e
Sai Naw <sainaw1331@gmail.com~>
2025-12-17 08:33:21 +0630

    Auto update

--- app/src/main/java/com/shan/tts/manager/AudioProcessor.kt
+++ app/src/main/java/com/shan/tts/manager/AudioProcessor.kt
@@ -1,19 +1,17 @@
 package com.shan.tts.manager
 
 object AudioProcessor {
-    // အစ်ကို့ရဲ့ CMakeLists.txt ထဲမှာ ပေးထားတဲ့ library နာမည်ကို ဒီမှာ ထည့်ရပါမယ်။
-    // ပုံမှန်အားဖြင့် "native-lib" သို့မဟုတ် "sai-nawkeyboard" ဖြစ်တတ်ပါတယ်။
     init {
         try {
-            System.loadLibrary("native-lib") 
+            System.loadLibrary("cherry-audio")
         } catch (e: UnsatisfiedLinkError) {
             e.printStackTrace()
         }
     }
 
-    // JNI Methods
-    external fun createProcessor(inRate: Int, outRate: Int, ch: Int)
-    external fun process(inData: ByteArray, len: Int): ByteArray
+    external fun initSonic(rate: Int, ch: Int)
+    external fun setConfig(speed: Float, pitch: Float)
+    external fun processAudio(inData: ByteArray, len: Int): ByteArray
     external fun flush()
 }
 
