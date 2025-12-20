c7fc6dcb44d0bd507c1269d260a4d042644e2412
Sai Naw <sainaw1331@gmail.com~>
2025-12-20 18:01:17 +0630

    Auto update

--- app/src/main/java/com/shan/tts/manager/CheckVoiceData.kt
+++ app/src/main/java/com/shan/tts/manager/CheckVoiceData.kt
@@ -13,19 +13,25 @@ class CheckVoiceData : Activity() {
         val availableVoices = ArrayList<String>()
         val unavailableVoices = ArrayList<String>()
 
-        availableVoices.add("shn-MM")
         availableVoices.add("shn")
-        
-        availableVoices.add("my-MM")
+        availableVoices.add("shn-MM")
+        availableVoices.add("shan")
+
         availableVoices.add("my")
-        
+        availableVoices.add("my-MM")
+        availableVoices.add("mya")
+        availableVoices.add("mya-MM")
+
+        availableVoices.add("en")
         availableVoices.add("en-US")
+        availableVoices.add("eng")
         availableVoices.add("eng-USA")
+        availableVoices.add("eng-GBR")
 
         val returnIntent = Intent()
         returnIntent.putStringArrayListExtra(TextToSpeech.Engine.EXTRA_AVAILABLE_VOICES, availableVoices)
         returnIntent.putStringArrayListExtra(TextToSpeech.Engine.EXTRA_UNAVAILABLE_VOICES, unavailableVoices)
-        
+
         setResult(TextToSpeech.Engine.CHECK_VOICE_DATA_PASS, returnIntent)
         finish()
     }
