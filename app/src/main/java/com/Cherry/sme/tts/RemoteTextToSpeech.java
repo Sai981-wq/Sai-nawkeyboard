53fedbc6866fe993819c2992ba03f6e2fc05c4ff
Sai Naw <sainaw1331@gmail.com~>
2026-02-07 21:17:02 +0630

    Auto update

--- app/src/main/java/com/Cherry/sme/tts/RemoteTextToSpeech.java
+++ app/src/main/java/com/Cherry/sme/tts/RemoteTextToSpeech.java
@@ -5,14 +5,9 @@ import android.speech.tts.TextToSpeech;
 
 public class RemoteTextToSpeech extends TextToSpeech {
 
-    // Constructor အဟောင်း (မသုံးတော့ပါ)
-    // public RemoteTextToSpeech(Context context, String engineName) { ... }
-
-    // Constructor အသစ် - Listener လက်ခံနိုင်အောင် ပြင်ထားသည်
-    public class RemoteTextToSpeech extends TextToSpeech {
-        public RemoteTextToSpeech(Context context, OnInitListener listener, String engineName) {
-            super(context, listener, engineName);
-        }
+    public RemoteTextToSpeech(Context context, OnInitListener listener, String engineName) {
+        super(context, listener, engineName);
     }
+
 }
 
